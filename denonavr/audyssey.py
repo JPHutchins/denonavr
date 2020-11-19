f1a1a6bde1084f86d40f555d42dae666db699118
J.P. Hutchins <jphutchins@gmail.com>
2020-11-18 19:54:28 -0800

    Generalize XML commands

--- denonavr/audyssey.py
+++ denonavr/audyssey.py
@@ -1,47 +1,32 @@
-"""Audyssey Settings."""
+"""AppCommand0300 Settings."""
 import logging
 from io import BytesIO
 import xml.etree.ElementTree as ET
 from requests.exceptions import ConnectTimeout, RequestException
 
-_LOGGER = logging.getLogger("Audyssey")
-
-MULTI_EQ_MAP = {"0": "Off", "1": "Flat", "2": "L/R Bypass", "3": "Reference"}
-MULTI_EQ_MAP_LABELS = dict((value, key) for key, value in MULTI_EQ_MAP.items())
-
-REF_LVL_OFFSET_MAP = {"0": "0dB", "1": "+5dB", "2": "+10dB", "3": "+15dB"}
-REF_LVL_OFFSET_MAP_LABELS = dict(
-    (value, key) for key, value in REF_LVL_OFFSET_MAP.items()
-)
-
-DYNAMIC_VOLUME_MAP = {"0": "Off", "1": "Light", "2": "Medium", "3": "Heavy"}
-DYNAMIC_VOLUME_MAP_LABELS = dict(
-    (value, key) for key, value in DYNAMIC_VOLUME_MAP.items()
-)
+_LOGGER = logging.getLogger("AppCommand0300")
 
 COMMAND_ENDPOINT = "/goform/AppCommand0300.xml"
 
 
-class Audyssey:
-    """Audyssey Settings."""
+class AppCommand0300:
+    """AppCommand0300 Base Class."""
 
     def __init__(self, receiver):
         """
-        Initialize Audyssey Settings of DenonAVR.
+        Initialize Command Settings of DenonAVR.
 
         :param receiver: DenonAVR Receiver
         :type receiver: DenonAVR
         """
         self.receiver = receiver
-
-        self.dynamiceq = None
-        self.dynamiceq_control = None
-        self.reflevoffset = None
-        self.reflevoffset_control = None
-        self.dynamicvol = None
-        self.dynamicvol_control = None
-        self.multeq = None
-        self.multeq_control = None
+        if self._params:
+            self.param_labels = {
+                param: {value: key for key, value in inner.items()}
+                for param, inner in self._params.items()
+            }
+        else:
+            _LOGGER.warning("Class AppCommand0300 initialized without params.")
 
     def send_command(self, xml_tree):
         """Send commands."""
@@ -62,6 +47,8 @@ class Audyssey:
         if result is None:
             return
 
+        _LOGGER.debug("Command:\n%s\nResponse:\n%s", body.getvalue(), result)
+
         try:
             # Return XML ElementTree
             return ET.fromstring(result)
@@ -78,9 +65,9 @@ class Audyssey:
         """Update settings."""
         root = ET.Element("tx")
         cmd = ET.SubElement(root, "cmd", id="3")
-        ET.SubElement(cmd, "name").text = "GetAudyssey"
+        ET.SubElement(cmd, "name").text = self._get_cmd_name
         param_list = ET.SubElement(cmd, "list")
-        for param in ["dynamiceq", "reflevoffset", "dynamicvol", "multeq"]:
+        for param in self._params.keys():
             ET.SubElement(param_list, "param", name=param)
         tree = ET.ElementTree(root)
 
@@ -94,29 +81,38 @@ class Audyssey:
             return False
 
         for param in audyssey_params:
-            if param.get("name") == "multeq":
-                self.multeq = MULTI_EQ_MAP.get(param.text)
-            elif param.get("name") == "dynamiceq":
-                self.dynamiceq = bool(int(param.text))
-            elif param.get("name") == "reflevoffset":
-                # Reference level offset can only be used with DynamicEQ
-                if self.dynamiceq is False:
-                    self.reflevoffset = False
-                else:
-                    self.reflevoffset = REF_LVL_OFFSET_MAP.get(param.text)
-            elif param.get("name") == "dynamicvol":
-                self.dynamicvol = DYNAMIC_VOLUME_MAP.get(param.text)
-            setattr(
-                self,
-                "{name}_control".format(name=param.get("name")),
-                bool(int(param.get("control"))),
-            )
+            name = param.get("name")
+            if not name:
+                continue
+
+            map_key = self._params.get(name)
+            if not map_key:
+                _LOGGER.warning("Unmapped name: %s", name)
+                continue
+
+            param_text = param.text
+            if not param_text:
+                _LOGGER.debug(
+                    "Parameter: %s has no text attribute (state). param control=%s",
+                    param,
+                    param.get("control"),
+                )
+                continue
+
+            param_state = map_key.get(param.text)
+            if not param_state:
+                _LOGGER.warning("State: %s not found in map for %s", param_text, name)
+                continue
+
+            setattr(self, name, param_state)
+            setattr(self, f"{name}_control", bool(int(param.get("control"))))
+
         return True
 
-    def _set_audyssey(self, parameter, value):
+    def _set(self, parameter, value):
         root = ET.Element("tx")
         cmd = ET.SubElement(root, "cmd", id="3")
-        ET.SubElement(cmd, "name").text = "SetAudyssey"
+        ET.SubElement(cmd, "name").text = self._set_cmd_name
         param_list = ET.SubElement(cmd, "list")
         ET.SubElement(param_list, "param", name=parameter).text = str(value)
         tree = ET.ElementTree(root)
@@ -130,21 +126,61 @@ class Audyssey:
 
         return False
 
+
+class Audyssey(AppCommand0300):
+    """Audyssey Commands."""
+
+    def __init__(self, receiver):
+        """
+        Initialize Audyssey Settings of DenonAVR.
+
+        :param receiver: DenonAVR Receiver
+        :type receiver: DenonAVR
+        """
+
+        self._set_cmd_name = "SetAudyssey"
+        self._get_cmd_name = "GetAudyssey"
+        self._params = {
+            "dynamiceq": {
+                "0": "Off",
+                "1": "On",
+            },
+            "reflevoffset": {
+                "0": "0dB",
+                "1": "+5dB",
+                "2": "+10dB",
+                "3": "+15dB",
+            },
+            "dynamicvol": {
+                "0": "Off",
+                "1": "Light",
+                "2": "Medium",
+                "3": "Heavy",
+            },
+            "multeq": {
+                "0": "Off",
+                "1": "Flat",
+                "2": "L/R Bypass",
+                "3": "Reference",
+            },
+        }
+        super().__init__(receiver)
+
     def dynamiceq_off(self):
         """Turn DynamicEQ off."""
-        if self._set_audyssey(parameter="dynamiceq", value=0) is True:
+        if self._set(parameter="dynamiceq", value=0) is True:
             self.dynamiceq = False
 
     def dynamiceq_on(self):
         """Turn DynamicEQ on."""
-        if self._set_audyssey(parameter="dynamiceq", value=1) is True:
+        if self._set(parameter="dynamiceq", value=1) is True:
             self.dynamiceq = True
 
-    def set_mutlieq(self, setting):
+    def set_multieq(self, setting):
         """Set MultiEQ mode."""
         if (
-            self._set_audyssey(
-                parameter="multeq", value=MULTI_EQ_MAP_LABELS.get(setting)
+            self._set(
+                parameter="multeq", value=self.param_labels["multeq"].get(setting)
             )
             is True
         ):
@@ -153,11 +189,12 @@ class Audyssey:
     def set_reflevoffset(self, setting):
         """Set Reference Level Offset."""
         # Reference level offset can only be used with DynamicEQ
+        # TODO: Let's take advantage of the "control" attr given by API
         if self.dynamiceq is True:
             if (
-                self._set_audyssey(
+                self._set(
                     parameter="reflevoffset",
-                    value=REF_LVL_OFFSET_MAP_LABELS.get(setting),
+                    value=self.param_labels["reflevoffset"].get(setting),
                 )
                 is True
             ):
@@ -166,10 +203,56 @@ class Audyssey:
     def set_dynamicvol(self, setting):
         """Set Dynamic Volume."""
         if (
-            self._set_audyssey(
+            self._set(
                 parameter="dynamicvol",
-                value=DYNAMIC_VOLUME_MAP_LABELS.get(setting),
+                value=self.param_labels["dynamicvol"].get(setting),
             )
             is True
         ):
             self.dynamicvol = setting
+
+
+class SurroundParameter(AppCommand0300):
+    """SurroundParameter Commands."""
+
+    def __init__(self, receiver):
+        """
+        Initialize Audyssey Settings of DenonAVR.
+
+        :param receiver: DenonAVR Receiver
+        :type receiver: DenonAVR
+        """
+
+        self._set_cmd_name = "SetSurroundParameter"
+        self._get_cmd_name = "GetSurroundParameter"
+        self._params = {
+            "dyncomp": {
+                "0": "Off",
+                "1": "Low",
+                "2": "Medium",
+                "3": "High",
+            },
+            "lfe": {str(gain): f"{str(gain)}dB" for gain in range(0, -11, -1)},
+        }
+        super().__init__(receiver)
+
+    def set_dyncomp(self, setting):
+        """Set Dolby Dynamic Compression mode."""
+        if (
+            self._set(
+                parameter="dyncomp", value=self.param_labels["dyncomp"].get(setting)
+            )
+            is True
+        ):
+            self.dyncomp = setting
+
+    def set_lfe(self, setting):
+        """Set Dynamic Volume."""
+        if (
+            self._set(
+                parameter="lfe",
+                value=self.param_labels["lfe"].get(setting),
+            )
+            is True
+        ):
+            self.lfe = setting
